69e689b7af03b5936ba2411fe87b4aff6ebd6220
Devotion Chikutuva <dvoch5@gmail.com>
2025-06-23 10:30:31 -0400

    added the hotel management project and more notes

---
+++ hotel_bookings/target/debug/deps/hotel_bookings-29e0e9aa7899c750.d
@@ -0,0 +1,5 @@
+/Users/devotionchikutuva/Documents/RUST/hotel_bookings/target/debug/deps/hotel_bookings-29e0e9aa7899c750: src/main.rs
+
+/Users/devotionchikutuva/Documents/RUST/hotel_bookings/target/debug/deps/hotel_bookings-29e0e9aa7899c750.d: src/main.rs
+
+src/main.rs:
---
+++ hotel_bookings/target/debug/hotel_bookings.d
@@ -0,0 +1,1 @@
+/Users/devotionchikutuva/Documents/RUST/hotel_bookings/target/debug/hotel_bookings: /Users/devotionchikutuva/Documents/RUST/hotel_bookings/src/main.rs
